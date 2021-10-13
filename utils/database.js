1b1cd6d9ec291943bd48774ec8b0533e13f631cc
chen945 <chen945@gms.tku.edu.tw>
2021-10-13 21:20:50 +0800

    update

--- utils/database.js
+++ utils/database.js
@@ -2,19 +2,33 @@ const { Pool } = require('pg');
 
 const isProduction = process.nev.MODE_ENV==="production"
 
-const pool = new Pool({
-  //user: 'postgres',
-  //host: 'localhost',
-  //database: 'crown_76',
-  //password: '0000',
-  //port: '5432',
+let pool;
 
-  connectionString: isProduction ? process.env.DATABASE_URL:`postgres://postgres:0000@localhost:5432/crowns76.`
-});
+if (isProduction){
+  pool =new pool({
+    connectionString:process.env.DATABASE_URL,
+    ssl: {PromiseRejectionEvent: false}
+  });
 
-pool.query('SELECT * from category', (err, res) => {
-    console.log(JSON.stringify(res.rows));
-    pool.end();
-  })
+}
+else{
+  pool=new pool ({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'crown_76',
+    password: '0000',
+    port: '5432',
+  });
+}
 
-  module.exports = pool;
+module.exports = pool;
+
+  //connectionString: isProduction ? process.env.DATABASE_URL:`postgres://postgres:0000@localhost:5432/crowns76.`
+
+
+//pool.query('SELECT * from category', (err, res) => {
+  //  console.log(JSON.stringify(res.rows));
+    //pool.end();
+  //})
+
+  
